7aa7ac96829f9af786d43811929c865c121b7f0a
Lucas Pécout <lucas.pecout@e-artsup.net>
2024-06-01 16:00:51 +0200

    tmpro + Input system

---
+++ SOHY/Assets/Script/Moi/Ennui/CubeList.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeList : MonoBehaviour
+{
+    public List<GameObject> cubeList;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject cube in cubeList)
+        {
+            foreach
+        }
+    }
+}
--- SOHY/Assets/Script/Moi/Ennui/RandomClickNewCube.cs
+++ SOHY/Assets/Script/Moi/Ennui/RandomClickNewCube.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RandomClickNewCube : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public void OnLeftClick(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.started)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            Physics.Raycast(ray, out hitInfo);
+            if (hitInfo.collider != null)
+            {
+                if (hitInfo.collider.CompareTag("CUBE"))
+                {
+
+                }
+            }
+        }
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
